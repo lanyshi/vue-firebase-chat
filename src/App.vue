a9f4be990c57ec92a2959b71be0088f8f319ffd2
Natalie Shi <lanyshi@outlook.com>
2021-05-31 15:48:56 -0400

    Add Cookie consent

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="app">
     <router-view/>
+    <footer>
+      <cookie-law buttonClass="btn btn-primary">
+        <div slot="message">
+          <p> This site uses 🍪 to ensure the best experience. </p>
+        </div>
+      </cookie-law>
+    </footer>
   </div>
 </template>
 
 <script>
+import CookieLaw from 'vue-cookie-law';
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    CookieLaw
+  },
 }
 </script>
 
@@ -18,5 +29,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.Cookie {
+  flex-flow: column;
 }
 </style>
