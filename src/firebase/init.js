05387f37c14a35cfccd76f27432baa8259d44256
Natalie Shi <lanyshi@outlook.com>
2021-07-13 21:20:18 -0400

    update firebase init.js

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 
 // Initialize Firebase
 var firebaseConfig = {
-  apiKey: "AIzaSyD9ZZimBlO_TOsK-ful0z9jWXmQBxlUwgY",
-  authDomain: "vue-chat-2c792.firebaseapp.com",
-  databaseURL: "https://vue-chat-2c792-default-rtdb.firebaseio.com",
-  projectId: "vue-chat-2c792",
-  storageBucket: "vue-chat-2c792.appspot.com",
-  messagingSenderId: "189371833610",
-  appId: "1:189371833610:web:5ea461f4225cd29e542ef3"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 // Initialize Firebase
 const myApp = firebase.initializeApp(firebaseConfig);
