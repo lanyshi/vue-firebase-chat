471146a2b8a891042028c94bb879b45dc7330b4d
Natalie Shi <lanyshi@outlook.com>
2021-12-31 20:26:03 -0500

    fixed firebase connection issue; updated tests for login

--- tests/components/Login.spec.js
+++ tests/components/Login.spec.js
@@ -2,13 +2,32 @@ import { createLocalVue, mount } from "@vue/test-utils"
 import VueCookies from 'vue-cookies'
 import Login from '@/components/Login.vue'
 
-test('properly handles username input', async () => {
-    const localVue = createLocalVue()
-    localVue.use(VueCookies)
+describe('Login.vue', () => {
+    it('empty username input', async () => {
+        const localVue = createLocalVue()
+        localVue.use(VueCookies)
 
-    const wrapper = mount(Login, {
-        localVue
+        const wrapper = mount(Login, {
+            localVue
+        })
+
+        await wrapper.find('form').trigger('submit.prevent')
+        expect(wrapper.vm.errors.usernameError).toBe("Please enter username.")
     })
 
-    await wrapper.find("button").trigger('click')
+    it('long username input', async () => {
+        const localVue = createLocalVue()
+        localVue.use(VueCookies)
+
+        const wrapper = mount(Login, {
+            localVue
+        })
+
+        const usernameInput = wrapper.find('input[name="name"]')
+        usernameInput.setValue("Lorem ipsum dolor sit amet, consectetur")
+        expect(wrapper.find('input[name="name"]').element.value).toBe("Lorem ipsum dolor sit amet, consectetur")
+
+        await wrapper.find('form').trigger('submit.prevent')
+        expect(wrapper.vm.errors.usernameError).toBe("Username can't be longer than 20 characters.")
+    })
 })
